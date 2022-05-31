58a9acddf57159f6480a4929336520cec5902de5
Everton <everton.pinheiro@outlook.com>
2022-05-31 17:22:12 -0300

    aula25

--- aula25/aula25.py
+++ aula25/aula25.py
@@ -2,6 +2,11 @@
 Split, Join, Enumerate
 * Split - Dividir uma string # str
 * Join - Juntar uma lista # str
-* Enumerate - Enumerar elementos da lista # list
+* Enumerate - Enumerar elementos da lista # iteraveis
 """
 
+string = "O Brasil é o pais do futebol, o Brasil é penta."
+lista = string.split(' ')
+
+
+print(lista)
