f0cdb2c788cfe4605c3246556da105b68b847e7e
Everton <everton.pinheiro@outlook.com>
2022-05-31 22:13:15 -0300

    aula26

--- aula25/aula25.py
+++ aula25/aula25.py
@@ -5,8 +5,24 @@ Split, Join, Enumerate
 * Enumerate - Enumerar elementos da lista # iteraveis
 """
 
+#usando split
 string = "O Brasil é o pais do futebol, o Brasil é penta."
-lista = string.split(' ')
+lista_1 = string.split(' ')
+lista_2 = string.split(',')
 
+print(lista_1)
+print(lista_2)
 
+#usando o join
+string2 = 'O Brasil é penta.'
+lista = string2.split(' ')
+string3 = ','.join(lista)
+
+print(string2)
 print(lista)
+print(string3)
+
+#usando enumerate ele retorna em tupla
+for indice, valor in enumerate(lista):
+    print()
+    print(indice, valor, lista[indice])
