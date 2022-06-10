c3fb5728d59a391b702b5fc29a5ce29b65f2ba1a
Everton <everton.pinheiro@outlook.com>
2022-06-10 09:44:48 -0300

    aula31
    resolução do desafio

--- aula31desafio/aula31.py
+++ aula31desafio/aula31.py
@@ -1,43 +1,53 @@
 """
-CPF = 313.813.148-48
---------------------------------------------------------------------
-3 * 10 = 30
-1 * 9 = 9
-3 * 8 = 24
-8 * 7 = 56
-1 * 6 = 6
-3 * 5 = 15
-1 * 4 = 4
-4 * 3 = 12
-8 * 2 = 16
-        172
-Primeiro Digito
-11 - (172 % 11) = 4
-11 > 9 = 0
-Digito 1 = 0
-
-
-3 * 11 = 33
-1 * 10 = 10
-3 * 9 = 27
-8 * 8 = 64
-1 * 7 = 7
-3 * 6 = 18
-1 * 5 = 5
-4 * 4 = 16
-8 * 3 = 24
-4 = 2 = 8
-        212
-
-11 - (212 % 11) = 4
-11 > 9 = 0
-Digito 2 = 8
+CPF = 168.995.350-09
+------------------------------------------------
+1 * 10 = 10           #    1 * 11 = 11 <-
+6 * 9  = 54           #    6 * 10 = 60
+8 * 8  = 64           #    8 *  9 = 72
+9 * 7  = 63           #    9 *  8 = 72
+9 * 6  = 54           #    9 *  7 = 63
+5 * 5  = 25           #    5 *  6 = 30
+3 * 4  = 12           #    3 *  5 = 15
+5 * 3  = 15           #    5 *  4 = 20
+0 * 2  = 0            #    0 *  3 = 0
+                      # -> 0 *  2 = 0
+         297          #             343
+11 - (297 % 11) = 11  #     11 - (343 % 11) = 9
+11 > 9 = 0            #
+Digito 1 = 0          #   Digito 2 = 9
 """
-#print(11 - (212 % 11))
 
+# Loop infinito
+while True:
+    # cpf = '16899535009'
+    cpf = input('Digite um CPF: ')
+    novo_cpf = cpf[:-2]                 # Elimina os dois últimos digitos do CPF
+    reverso = 10                        # Contador reverso
+    total = 0
 
-novo_cpf = input('Digite somente os numeros do seu CPF' )
-if cpf == novo_cpf:
-    print('CPF Válido')
-else:
-    print('CPF Inválido')
+    # Loop do CPF
+    for index in range(19):
+        if index > 8:                   # Primeiro índice vai de 0 a 9,
+            index -= 9                  # São os 9 primeiros digitos do CPF
+
+        total += int(novo_cpf[index]) * reverso  # Valor total da multiplicação
+
+        reverso -= 1                    # Decrementa o contador reverso
+        if reverso < 2:
+            reverso = 11
+            d = 11 - (total % 11)
+
+            if d > 9:                   # Se o digito for > que 9 o valor é 0
+                d = 0
+            total = 0                   # Zera o total
+            novo_cpf += str(d)          # Concatena o digito gerado no novo cpf
+
+    # Evita sequencias. Ex.: 11111111111, 00000000000...
+    sequencia = novo_cpf == str(novo_cpf[0]) * len(cpf)
+
+    # Descobri que sequências avaliavam como verdadeiro, então também
+    # adicionei essa checagem aqui
+    if cpf == novo_cpf and not sequencia:
+        print('Válido')
+    else:
+        print('Inválido')
